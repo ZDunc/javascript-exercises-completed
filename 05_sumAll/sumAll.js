f2476ec7569340b3f03fa34621796848d7d2015e
ZDunc <zacharyduncan@gmail.com>
2022-05-30 20:25:31 -0600

    Completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+    let sum = 0;
 
+    if (isNaN(low) || isNaN(high) || typeof low === 'string' || typeof high === 'string')
+        return 'ERROR';
+    else if (low < 0 || high < 0)
+        return 'ERROR';
+    else {
+        if (low > high) {
+            let temp = low;
+            low = high;
+            high = temp;
+        }
+
+        for (let i = low; i <= high; i++) {
+            sum += i;
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
