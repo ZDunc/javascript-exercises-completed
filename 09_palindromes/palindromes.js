8202940b35f8f9376d3d0e57f4ba745b5a37f4c9
ZDunc <zacharyduncan@gmail.com>
2022-05-30 22:15:47 -0600

    Completed exericse 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let nopunctuation = string.replace(/\W/g,"");
+    let nospaces = nopunctuation.replace(/\s/g,"");
+    string = nospaces;
 
+    for (let i = 0; i < string.length / 2; i++) {
+
+        if (string[i].toLowerCase() !== string[string.length - i - 1].toLowerCase())
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
