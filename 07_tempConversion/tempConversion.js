3154487b3c6d157a4fe67fc8a437e7070f909300
ZDunc <zacharyduncan@gmail.com>
2022-05-30 20:34:55 -0600

    Conmpleted exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(int) {
+  int = (int - 32)*(5/9);
+  int = Math.round(int * 10) / 10;
+  return int;
 };
 
-const ctof = function() {
-
+const ctof = function(int) {
+  int = (int * (9/5)) + 32;
+  int = Math.round(int * 10) / 10;
+  return int;
 };
 
 // Do not edit below this line
