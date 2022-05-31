ce5af80adc8aa0ac2e2176de5244e661a1aa0543
ZDunc <zacharyduncan@gmail.com>
2022-05-30 20:01:08 -0600

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+// removes specified elements from an array, 
+// Pass in one array in arg[0] and unlimited element arguments
+const removeFromArray = function(arr) {
+    let args = Array.from(arguments);
 
+    args.splice(0, 1);
+    console.log(arr);
+    console.log(args);
+
+    for (let i = 0; i < args.length; i++) {
+        index = arr.indexOf(args[i]);
+        if (index === -1)
+            continue;
+
+        arr.splice(index, 1);
+    }
+
+    console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
