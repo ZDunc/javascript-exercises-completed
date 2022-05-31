d4dd97e8544df39f97af8ec17a4169a9fa3ee2e1
ZDunc <zacharyduncan@gmail.com>
2022-05-30 21:41:02 -0600

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  for (let int in arr) {
+    sum += arr[int];
+  }
+  return sum;
+}
+
+const multiply = function(arr) {
+  let product = 1;
+  for (let int in arr) {
+    product *= arr[int];
+  }
+  return product;
 };
 
-const multiply = function() {
-
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const power = function() {
-	
-};
+const factorial = function(number) {
+  let result = 1;
 
-const factorial = function() {
-	
+  for (let i = 2; i <= number; i++) {
+    result = result * i;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
