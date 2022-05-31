f4e50347b17f2de40073fa4620ffa590fd76f27a
ZDunc <zacharyduncan@gmail.com>
2022-05-30 22:57:04 -0600

    Conpleted exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest;
+    let oldestAge = 0;
 
+    for (let i = 0; i < people.length; i++) {
+        let age;
+        if (people[i].yearOfDeath == undefined)
+            age = 2022 - people[i].yearOfBirth;
+        else 
+            age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+        if (age > oldestAge) {
+            oldest = people[i];
+            oldestAge = age;
+        }
+    }
+
+    return oldest;
 };
 
 // Do not edit below this line
